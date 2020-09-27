6468e8fc10658134aef7a051f0ab890094cedb3e
UncleH <ecomerim1@gmail.com>
2020-09-27 20:37:33 +0300

    Login Design Part 3, Animation Color Change Logic

--- app/src/main/java/com/halit/instaghalit/LoginActivity.java
+++ app/src/main/java/com/halit/instaghalit/LoginActivity.java
@@ -1,13 +1,44 @@
 package com.halit.instaghalit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 
 public class LoginActivity extends AppCompatActivity {
+    LinearLayout mLoginContainer;
+    AnimationDrawable mAnimationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        //animation drawable variable & config
+        mLoginContainer = (LinearLayout) findViewById(R.id.login_container);
+        mAnimationDrawable = (AnimationDrawable) mLoginContainer.getBackground();
+        mAnimationDrawable.setEnterFadeDuration(5000);
+        mAnimationDrawable.setExitFadeDuration(2000);
+
+    }
+
+    @Override
+    protected void onPostResume() {
+
+        super.onPostResume();
+
+        if (mAnimationDrawable != null && mAnimationDrawable.isRunning()) {
+            mAnimationDrawable.start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mAnimationDrawable != null && mAnimationDrawable.isRunning()) {
+            mAnimationDrawable.stop();
+        }
     }
 }
