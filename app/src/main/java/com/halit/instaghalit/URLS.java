1c1fcfa01d1c22dad61472809ee390529f5b776f
UncleH <ecomerim1@gmail.com>
2020-09-28 00:57:37 +0300

    Login Logic Part-2 & Part-3 & Part-4

---
+++ app/src/main/java/com/halit/instaghalit/URLS.java
@@ -0,0 +1,12 @@
+package com.halit.instaghalit;
+
+/**
+ * Created by android on 9/28/20.
+ */
+
+public class URLS {
+
+    public static final String login_api = "https://cikisgiris.000webhostapp.com/login.php";
+    public static final String sign_up_api = "https://cikisgiris.000webhostapp.com/sign_up.php";
+
+}
---
+++ app/src/main/java/com/halit/instaghalit/User.java
@@ -0,0 +1,49 @@
+package com.halit.instaghalit;
+
+/**
+ * Created by android on 9/28/20.
+ */
+
+public class User {
+
+    String email,username,image;
+    int id;
+
+    public User(int id,String email, String username) {
+        this.email = email;
+        this.username = username;
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
