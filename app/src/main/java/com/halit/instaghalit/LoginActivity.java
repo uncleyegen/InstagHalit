8fcd6c02f036c2d2107146725cc8c35a0fd5b069
UncleH <ecomerim1@gmail.com>
2020-09-28 00:46:37 +0300

    Login Logic Part 1

--- app/src/main/java/com/halit/instaghalit/LoginActivity.java
+++ app/src/main/java/com/halit/instaghalit/LoginActivity.java
@@ -1,13 +1,25 @@
 package com.halit.instaghalit;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
     LinearLayout mLoginContainer;
     AnimationDrawable mAnimationDrawable;
+
+    EditText username_et, password_et;
+    TextView sign_up_btn,forgot_pass_btn;
+    Button login_btn;
+    ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +28,45 @@ public class LoginActivity extends AppCompatActivity {
 
 
         //animation drawable variable & config
-        mLoginContainer = (LinearLayout) findViewById(R.id.login_container);
+        mLoginContainer = findViewById(R.id.login_container);
         mAnimationDrawable = (AnimationDrawable) mLoginContainer.getBackground();
         mAnimationDrawable.setEnterFadeDuration(5000);
         mAnimationDrawable.setExitFadeDuration(2000);
+
+        username_et = findViewById(R.id.user_name);
+        password_et = findViewById(R.id.user_password);
+        sign_up_btn = findViewById(R.id.sign_up_btn);
+        login_btn = findViewById(R.id.login_btn);
+        forgot_pass_btn = findViewById(R.id.forgot_pass_btn);
+        mProgressDialog = new ProgressDialog(this);
+
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logIn();
+            }
+        });
+
+        sign_up_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                Intent signIntent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(signIntent);
+            }
+        });
+
+        forgot_pass_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    private void logIn() {
 
     }
 
