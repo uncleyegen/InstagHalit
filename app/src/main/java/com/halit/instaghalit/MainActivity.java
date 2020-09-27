4fae07d17019edcd1b34b38cb55460a1edcfde12
UncleH <ecomerim1@gmail.com>
2020-09-27 14:23:01 +0300

    Navigation View Click Listener

--- app/src/main/java/com/halit/instaghalit/MainActivity.java
+++ app/src/main/java/com/halit/instaghalit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.halit.instaghalit;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -7,10 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout mDrawerLayout;
     ActionBarDrawerToggle mActionDrawerToggle;
+    NavigationView mNavigationView;
 
 
     @Override
@@ -20,11 +23,44 @@ public class MainActivity extends AppCompatActivity {
 // Navigation Drawer to be opened from right to left (minSdkVersion 17) // Add this code to manifest: <application android:supportsRtl="true">
 //        getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
 
+        // Layout Variables
         mDrawerLayout = findViewById(R.id.main_drawer_layout);
+        mNavigationView = findViewById(R.id.main_nav_view);
+
         mActionDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mActionDrawerToggle);
         mActionDrawerToggle.syncState();
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        // Listener for navigation View
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId() == R.id.home){
+                    Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                    return true;
+                }else if (item.getItemId() == R.id.search){
+                    Toast.makeText(MainActivity.this, "Search", Toast.LENGTH_SHORT).show();
+                    return true;
+                }else if (item.getItemId() == R.id.camera){
+                    Toast.makeText(MainActivity.this, "Camera", Toast.LENGTH_SHORT).show();
+                    return true;
+                }else if (item.getItemId() == R.id.likes){
+                    Toast.makeText(MainActivity.this, "Likes", Toast.LENGTH_SHORT).show();
+                    return true;
+                }else if (item.getItemId() == R.id.profile){
+                    Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                    return true;
+                }else if (item.getItemId() == R.id.log_out){
+                    Toast.makeText(MainActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+                }
+
+                return false;
+
+            }
+        });
 
     }
 
