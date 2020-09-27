52ab0eb9fad8db9c1f0c21f88b6097ecec0255e2
UncleH <ecomerim1@gmail.com>
2020-09-27 13:31:03 +0300

    8. Navigation Drawer Part 2
    Change Icon,activity_main.xml, MainActivity.java

--- app/src/main/java/com/halit/instaghalit/MainActivity.java
+++ app/src/main/java/com/halit/instaghalit/MainActivity.java
@@ -1,13 +1,29 @@
 package com.halit.instaghalit;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle mActionDrawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+// Navigation Drawer to be opened from right to left (minSdkVersion 17) // Add this code to manifest: <application android:supportsRtl="true">
+//        getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+
+        mDrawerLayout = findViewById(R.id.main_drawer_layout);
+        mActionDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mActionDrawerToggle);
+        mActionDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
