2a556ed8ef2a96ef5d4e71fdf81c2ed3a0eb49c2
UncleH <ecomerim1@gmail.com>
2020-09-29 22:13:27 +0300

    Shared Preference - Store Users Data

---
+++ app/src/main/java/com/halit/instaghalit/SharedPrefrenceManger.java
@@ -0,0 +1,46 @@
+package com.halit.instaghalit;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by android on 9/29/20.
+ */
+
+public class SharedPrefrenceManger {
+
+    private static final String FILENAME = "INSTAGRAMLOGIN";
+    private static final String USERNAME = "username";
+    private static final String EMAIL = "email";
+    private static final String ID = "id";
+
+    private static SharedPrefrenceManger mSharedPrefreneManger;
+    private static Context mContext;
+
+    private SharedPrefrenceManger(Context context) {
+        this.mContext = context;
+    }
+
+
+    public static synchronized SharedPrefrenceManger getInstance(Context context){
+
+        if(mSharedPrefreneManger == null){
+            mSharedPrefreneManger = new SharedPrefrenceManger(context);
+        }
+        return mSharedPrefreneManger;
+    }
+
+
+    public void storeUserData(User user){
+        SharedPreferences sharedPreferences = mContext.getSharedPreferences(FILENAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(USERNAME,user.getUsername());
+        editor.putString(EMAIL,user.getEmail());
+        editor.putInt(ID,user.getId());
+        editor.apply();
+
+    }
+
+
+}
+
