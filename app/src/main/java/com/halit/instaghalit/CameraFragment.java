33910a49c469648f5bf09a74a819f14608f31273
UncleH <ecomerim1@gmail.com>
2020-10-03 00:55:39 +0300

    Capture Image Click Listener

--- app/src/main/java/com/halit/instaghalit/CameraFragment.java
+++ app/src/main/java/com/halit/instaghalit/CameraFragment.java
@@ -1,13 +1,20 @@
 package com.halit.instaghalit;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class CameraFragment extends Fragment {
+
+    Button upload_btn,capture_btn;
+    ImageView captured_iv;
 
 
     public CameraFragment() {
@@ -27,10 +34,45 @@ public class CameraFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false);
+
+        View view =  inflater.inflate(R.layout.fragment_camera, container, false);
+
+        upload_btn = view.findViewById(R.id.upload_btn);
+        capture_btn =  view.findViewById(R.id.capture_btn);
+        captured_iv =  view.findViewById(R.id.captured_iv);
+
+        return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        capture_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                capturePhoto();
+
+            }
+        });
+
+        upload_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                storyAndImageTitle();
+
+            }
+        });
 
 
+    }
+
+    private void capturePhoto() {
+
+    }
+
+    private void storyAndImageTitle() {
+
+    }
 }
