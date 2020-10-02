a77a420c4466f8e086d3d2bdac418d3d09e16dca
UncleH <ecomerim1@gmail.com>
2020-10-03 01:17:57 +0300

    Capture Photo Method Activity Result

--- app/src/main/java/com/halit/instaghalit/CameraFragment.java
+++ app/src/main/java/com/halit/instaghalit/CameraFragment.java
@@ -1,6 +1,7 @@
 package com.halit.instaghalit;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -8,6 +9,7 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class CameraFragment extends Fragment {
@@ -23,6 +28,7 @@ public class CameraFragment extends Fragment {
     ImageView captured_iv;
     Uri mImageUri;
     final int CAPTURE_IMAGE = 1,GALLARY_PICK = 2;
+    Bitmap bitmap;
 
 
     public CameraFragment() {
@@ -83,6 +89,33 @@ public class CameraFragment extends Fragment {
         mImageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),imageName));
         cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
         startActivityForResult(cameraIntent, CAPTURE_IMAGE);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+
+        if(requestCode == CAPTURE_IMAGE && resultCode == RESULT_OK){
+
+            if(mImageUri != null) {
+                Log.i("imageuri",mImageUri.getPath());
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(),mImageUri);
+                    if(bitmap != null){
+                        captured_iv.setImageBitmap(bitmap);
+                    }
+                    Log.i("bitmap",bitmap.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
+
 
     }
 
