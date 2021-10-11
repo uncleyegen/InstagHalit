a0b5e65858d0be11377344858fcba6d601314cc4
UncleH <ecomerim1@gmail.com>
2021-10-11 21:58:01 +0300

    web service

--- app/src/main/java/com/halit/instaghalit/URLS.java
+++ app/src/main/java/com/halit/instaghalit/URLS.java
@@ -7,12 +7,15 @@ package com.halit.instaghalit;
 public class URLS {
 
 //    public static final String login_api   = "https://cikisgiris.000webhostapp.com/InstagHalit/login.php";
+//    public static final String sign_up_api = "https://cikisgiris.000webhostapp.com/InstagHalit/sign_up.php";
+//    public static final String upload_story_image = "https://cikisgiris.000webhostapp.com/InstagHalit/upload_story_image.php";
+//    public static final String get_user_data = "http://cikisgiris.000webhostapp.com/get_user_data.php?user_id=";
 
 
-    public static final String login_api   = "https://cikisgiris.000webhostapp.com/InstagHalit/login.php";
-    public static final String sign_up_api = "https://cikisgiris.000webhostapp.com/InstagHalit/sign_up.php";
-    public static final String upload_story_image = "https://cikisgiris.000webhostapp.com/InstagHalit/upload_story_image.php";
-    public static final String get_user_data = "http://cikisgiris.000webhostapp.com/get_user_data.php?user_id=";
+    public static final String login_api   = "http://www.termhalit.com/InstagHalit/login.php";
+    public static final String sign_up_api = "http://www.termhalit.com/InstagHalit/sign_up.php";
+    public static final String upload_story_image = "http://www.termhalit.com/InstagHalit/upload_story_image.php";
+    public static final String get_user_data = "http://www.termhalit.com/get_user_data.php?user_id=";
 
 
 //                                             for LocalHOST add your computer IP address
